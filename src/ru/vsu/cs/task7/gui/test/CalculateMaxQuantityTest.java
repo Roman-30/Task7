479e0a0440f83b4445105af0d5e7c85bec32fb13
Roman-30 <71034963+Roman-30@users.noreply.github.com>
2020-12-14 12:45:09 +0300

    Update CalculateMaxQuantityTest.java

--- src/ru/vsu/cs/task7/gui/test/CalculateMaxQuantityTest.java
+++ src/ru/vsu/cs/task7/gui/test/CalculateMaxQuantityTest.java
@@ -2,76 +2,76 @@ package ru.vsu.cs.task7.gui.test;
 
 import org.junit.Assert;
 import org.junit.Test;
-import ru.vsu.cs.task7.gui.logics.CalculateMaxQuantity;
+import ru.vsu.cs.task7.gui.logics.QuantityCalculate;
 
 public class CalculateMaxQuantityTest {
 
     @Test
     public void testProgram0() {
-        int array = CalculateMaxQuantity.findMaximumNumberRepetitions(new int[]{1, 4, 1});
+        int array = QuantityCalculate.findMaximumNumberRepetitions(new int[]{1, 4, 1});
         int quantity = 2;
         Assert.assertEquals(quantity, array);
     }
 
     @Test
     public void testProgram1() {
-        int array = CalculateMaxQuantity.findMaximumNumberRepetitions(new int[]{1, 1, 1, 1, 1, 1});
+        int array = QuantityCalculate.findMaximumNumberRepetitions(new int[]{1, 1, 1, 1, 1, 1});
         int quantity = 6;
         Assert.assertEquals(quantity, array);
     }
 
     @Test
     public void testProgram2() {
-        int array = CalculateMaxQuantity.findMaximumNumberRepetitions(new int[]{1, 2, 3, 4, 5, 6, 6});
+        int array = QuantityCalculate.findMaximumNumberRepetitions(new int[]{1, 2, 3, 4, 5, 6, 6});
         int quantity = 2;
         Assert.assertEquals(quantity, array);
     }
 
     @Test
     public void testProgram3() {
-        int array = CalculateMaxQuantity.findMaximumNumberRepetitions(new int[]{1, 1, 2, 2, 3, 3});
+        int array = QuantityCalculate.findMaximumNumberRepetitions(new int[]{1, 1, 2, 2, 3, 3});
         int quantity = 2;
         Assert.assertEquals(quantity, array);
     }
 
     @Test
     public void testProgram4() {
-        int array = CalculateMaxQuantity.findMaximumNumberRepetitions(new int[]{1, 2, 3, 4, 5, 6});
+        int array = QuantityCalculate.findMaximumNumberRepetitions(new int[]{1, 2, 3, 4, 5, 6});
         int quantity = 1;
         Assert.assertEquals(quantity, array);
     }
 
     @Test
     public void testProgram5() {
-        int array = CalculateMaxQuantity.findMaximumNumberRepetitions(new int[]{});
+        int array = QuantityCalculate.findMaximumNumberRepetitions(new int[]{});
         int quantity = 0;
         Assert.assertEquals(quantity, array);
     }
 
     @Test
     public void testProgram6() {
-        int array = CalculateMaxQuantity.findMaximumNumberRepetitions(new int[]{1, 1, 1, 2, 2});
+        int array = QuantityCalculate.findMaximumNumberRepetitions(new int[]{1, 1, 1, 2, 2});
         int quantity = 3;
         Assert.assertEquals(quantity, array);
     }
 
     @Test
     public void testProgram7() {
-        int array = CalculateMaxQuantity.findMaximumNumberRepetitions(new int[]{-1, -1, 0, -5, -99, -19999, -1});
+        int array = QuantityCalculate.findMaximumNumberRepetitions(new int[]{-1, -1, 0, -5, -99, -19999, -1});
         int quantity = 3;
         Assert.assertEquals(quantity, array);
     }
 
     @Test
     public void testProgram8() {
-        int array = CalculateMaxQuantity.findMaximumNumberRepetitions(new int[]{11, 22, 34, 39, 12, 99, 11, 12});
+        int array = QuantityCalculate.findMaximumNumberRepetitions(new int[]{11, 22, 34, 39, 12, 99, 11, 12});
         int quantity = 2;
         Assert.assertEquals(quantity, array);
     }
 
     @Test
     public void testProgram9() {
-        int array = CalculateMaxQuantity.findMaximumNumberRepetitions(new int[]{1, 2, 1, 2, 1, 2, 1});
+        int array = QuantityCalculate.findMaximumNumberRepetitions(new int[]{1, 2, 1, 2, 1, 2, 1});
         int quantity = 4;
         Assert.assertEquals(quantity, array);
     }
